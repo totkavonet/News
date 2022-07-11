992151adc8615aab7db4407b46670651f5a21807
PavelLoiko <loikapavel2405@gmail.com>
2022-07-11 23:38:50 +0300

    devise configured add trackable, has_many for roles, created at in show

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,11 @@ ActiveRecord::Schema.define(version: 2022_07_11_202003) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
