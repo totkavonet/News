7132f2f3edf7c36c2c7f614d08cbe219afe897e1
PavelLoiko <loikapavel2405@gmail.com>
2022-07-13 01:05:15 +0300

    implement functionality to hide news v2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   # before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: :index
-  load_and_authorize_resource except: [:index, :all, :discarded]
+  load_and_authorize_resource except: [:index]
 
 
   # GET /posts or /posts.json
