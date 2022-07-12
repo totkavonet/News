b01c48c3f7514144d1ee5af5e0ca845993ffa344
PavelLoiko <loikapavel2405@gmail.com>
2022-07-12 23:03:01 +0300

    add upload image functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.png\Z/
+  # validates_attachment_content_type 'image/png', 'image/jpg'
+  validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|)$/, :message => 'file type is not allowed (only jpg/png images)'
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_202003) do
+ActiveRecord::Schema.define(version: 2022_07_12_194717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2022_07_11_202003) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image_data"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
